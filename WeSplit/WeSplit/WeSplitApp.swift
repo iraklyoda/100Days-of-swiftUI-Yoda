b5f51ba7e92843c134b879b6dc9cb3a3a5bb9f00
Irakli <Iraklyoda42@gmail.com>
2024-07-08 01:50:23 +0400

    init: Initiatize WeSplit Ios App Project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Irakli Iobashvili on 08.07.24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
